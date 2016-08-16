75767326e29b54dad189ac805612e29e4681acd7
robertgates55 <robertgates55@gmail.com>
2016-08-16 16:32:58 +0100

    Further updates to improve allure output

--- src/test/java/net/moodel/demo/utils/CustomCukeListener.java
+++ src/test/java/net/moodel/demo/utils/CustomCukeListener.java
@@ -4,21 +4,28 @@ import cucumber.runtime.StepDefinitionMatch;
 import gherkin.formatter.Formatter;
 import gherkin.formatter.Reporter;
 import gherkin.formatter.model.*;
+import gherkin.formatter.model.Step;
 import ru.yandex.qatools.allure.Allure;
 import ru.yandex.qatools.allure.config.AllureModelUtils;
 import ru.yandex.qatools.allure.events.*;
+import ru.yandex.qatools.allure.testng.AllureTestListener;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class CustomCukeListener implements Formatter, Reporter {
 
     private Allure lifecycle = Allure.LIFECYCLE;
-
     public Allure getLifecycle() {
         return lifecycle;
     }
 
     private String featureInUse;
+    private Boolean logSteps = true;
+    private Boolean logScenarioAsTest = false;
+    private Boolean logScenarioAsStep = true;
+    private int stepBrokenCount;
+    private Throwable latestError;
 
     @Override
     public void syntaxError(String s, String s1, List<String> list, String s2, Integer integer) {
@@ -32,15 +39,11 @@ public class CustomCukeListener implements Formatter, Reporter {
 
     @Override
     public void feature(Feature feature) {
-
         featureInUse = feature.getName();
-//        TestSuiteStartedEvent suiteStartedEvent = new TestSuiteStartedEvent(feature.getName(), feature.getName());
-//        getLifecycle().fire(suiteStartedEvent);
     }
 
     @Override
     public void scenarioOutline(ScenarioOutline scenarioOutline) {
-        System.out.println(scenarioOutline.toString());
     }
 
     @Override
@@ -50,20 +53,22 @@ public class CustomCukeListener implements Formatter, Reporter {
 
     @Override
     public void startOfScenarioLifeCycle(Scenario scenario) {
-        TestCaseStartedEvent scenStarterEvent = new TestCaseStartedEvent(featureInUse, scenario.getName())
-                .withLabels(
-                        AllureModelUtils.createFeatureLabel(featureInUse),
-                        AllureModelUtils.createStoryLabel(scenario.getName())
-//                        AllureModelUtils.createTestLabel(scenario.getName())
-                );
-        getLifecycle().fire(scenStarterEvent);
+        stepBrokenCount=0;
+        if(logScenarioAsTest) {
+            getLifecycle().fire(new TestCaseStartedEvent(featureInUse, scenario.getName())
+                    .withLabels(
+                            AllureModelUtils.createFeatureLabel(featureInUse),
+                            AllureModelUtils.createStoryLabel(scenario.getName())
+                    ));
+        } else if (logScenarioAsStep){
+            getLifecycle().fire(new StepStartedEvent("[SCN] " + scenario.getName()));
+        }
     }
 
     @Override
     public void background(Background background) {
 
     }
-
 
     @Override
     public void scenario(Scenario scenario) {
@@ -72,19 +77,27 @@ public class CustomCukeListener implements Formatter, Reporter {
 
     @Override
     public void step(Step step) {
-        System.out.println("STEP! - " + step.getKeyword() + " " + step.getName() + " " + step.hashCode() );
+
     }
 
     @Override
     public void endOfScenarioLifeCycle(Scenario scenario) {
-        TestCaseFinishedEvent scenFinishedEvent = new TestCaseFinishedEvent();
-        getLifecycle().fire(scenFinishedEvent);
+        if(logScenarioAsTest) {
+            if (stepBrokenCount > 0) {
+                getLifecycle().fire(new TestCaseFailureEvent().withThrowable(latestError));
+            }
+            getLifecycle().fire(new TestCaseFinishedEvent());
+        } else if (logScenarioAsStep){
+            if (stepBrokenCount > 0) {
+                getLifecycle().fire(new StepFailureEvent().withThrowable(latestError));
+            }
+            getLifecycle().fire(new StepFinishedEvent());
+        }
     }
 
     @Override
     public void done() {
-//        TestSuiteFinishedEvent testSuiteFinishedEvent = new TestSuiteFinishedEvent(featureInUse);
-//        getLifecycle().fire(testSuiteFinishedEvent);
+
     }
 
     @Override
@@ -97,28 +110,28 @@ public class CustomCukeListener implements Formatter, Reporter {
 
     }
 
-
-
     @Override
     public void before(Match match, Result result) {
-        System.out.println("STEP_before");
-        StepEvent stepEvent = new StepStartedEvent("START!");
-        getLifecycle().fire(stepEvent);
+
     }
 
     @Override
     public void result(Result result) {
-        System.out.println("STEP_after");
-        if(result.getStatus() != "passed"){
-            StepFailureEvent stepFailureEvent = new StepFailureEvent().withThrowable(result.getError());
-            getLifecycle().fire(stepFailureEvent);
-        } else {
-            StepFinishedEvent stepFinishedEvent = new StepFinishedEvent();
-            getLifecycle().fire(stepFinishedEvent);
+
+        if (result.getStatus() != "undefined") {
+
+            if (result.getStatus() != "passed") {
+                stepBrokenCount++;
+                latestError = result.getError();
+            }
+
+            if (logSteps) {
+                if (result.getStatus() != "passed") {
+                    getLifecycle().fire(new StepFailureEvent().withThrowable(result.getError()));
+                }
+                getLifecycle().fire(new StepFinishedEvent());
+            }
         }
-        System.out.println("STEPSTOP: " + System.currentTimeMillis());
-
-
     }
 
     @Override
@@ -128,30 +141,23 @@ public class CustomCukeListener implements Formatter, Reporter {
 
     @Override
     public void match(Match match) {
-
-        StepDefinitionMatch step = (StepDefinitionMatch)match;
-//        StepStartedEvent stepStartedEvent = new StepStartedEvent(step.getStepName());
-//        ru.yandex.qatools.allure.model.Step step1 = new ru.yandex.qatools.allure.model.Step()
-//                .withName(step.getStepName())
-//                .withStart(System.currentTimeMillis());
-        StepEvent stepEvent = new StepStartedEvent(step.getStepName());
-        getLifecycle().fire(stepEvent);
-        System.out.println("STEPSTART: " + System.currentTimeMillis() + " " + step.getStepName());
-
-//
-//        StepFinishedEvent stepFinishedEvent = new StepFinishedEvent();
-//        getLifecycle().fire(stepFinishedEvent);
+        if (logSteps) {
+            if(match.getLocation() != null) {
+                StepDefinitionMatch stepDef = (StepDefinitionMatch) match;
+                Step step = (Step) getFieldValueInObject(stepDef, "step");
+                getLifecycle().fire(new StepStartedEvent(step.getKeyword() + " " + step.getName()));
+            }
+        }
     }
 
     @Override
     public void embedding(String s, byte[] bytes) {
-        System.out.println("Reporter - EMBEDDING!");
 
     }
 
     @Override
     public void write(String s) {
-        System.out.println("Reporter - WRITE!");
+
     }
 
 
@@ -160,14 +166,18 @@ public class CustomCukeListener implements Formatter, Reporter {
 
 
 
-
-
-
-
-
-
-
-
+    private <T> Object getFieldValueInObject(T m, String field) {
+        try {
+            Field fieldInObject = getFieldInObject(m, field);
+            fieldInObject.setAccessible(true);
+            return fieldInObject.get(m);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+    private <T> Field getFieldInObject(T m, String field) throws NoSuchFieldException {
+        return m.getClass().getDeclaredField(field);
+    }
 
 
 
